e8b4c604a313a94565d063d150e9d838ead27a79
Talha Demir <118924543+Talha-Fe@users.noreply.github.com>
2025-02-26 18:25:03 +0300

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
 
+        if(nums.length >= 2 && nums.length <= Math.pow(10,4)){
+
+            for(int i = 0; i < nums.length; i++){
+
+                for(int j = i+1; j < nums.length; j++){
+
+                    if(nums[j]+nums[i] == target ) {
+
+                        return new int[]{i, j};
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return new int[]{};
+
+    }
+
+}
